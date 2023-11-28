29cbc78c7bf16b547fb7af6daae7a1019dd49bcc
Игорь Бородий <Igoruxa007@gmail.com>
2023-11-28 17:04:54 +0300

    ex 4

--- level_1/4.py
+++ level_1/4.py
@@ -1,11 +1,14 @@
 import datetime
+from dateutil.relativedelta import relativedelta
 
-from constants import ___
+from constants import variable
 
 
-def calculate_age(date_of_birth: ___) -> ___:
-    pass
+def calculate_age(date_of_birth: datetime.date) -> int:
+    a = datetime.date.today()
+    b = relativedelta(a, date_of_birth).years
+    return b
 
 
 if __name__ == "__main__":
-    assert calculate_age(date_of_birth=datetime.date(1965, 6, 2)) == 57
+    assert calculate_age(date_of_birth=datetime.date(1965, 6, 2)) == 58
